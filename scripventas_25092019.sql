d5786c6994e5ac3bcd768ba478696e7f5fe6beca
lab01 <>
2019-09-25 15:58:20 -0500

    el escript para la creacion de la tabla ventas

---
+++ scripventas_25092019.sql
@@ -0,0 +1,43 @@
+﻿create table tipo_cliente(
+codtipo serial not null primary key,
+nombre varchar(20) not null
+)
+select * from tipo_cliente
+insert into tipo_cliente(nombre) values ('Persona natural RUC')
+
+drop table cliente
+
+create table cliente(
+codcliente int not null primary key,
+dni char(8) null,
+ruc char(11) null, 
+nombres varchar(30) not null, 
+telefono varchar(13) null,
+correo varchar (50) null,
+direccion varchar(70) not null,
+vigencia boolean not null,
+codtipo int not null references tipo_cliente(codtipo)
+)
+
+create table venta(
+numventa int not null primary key,
+fecha date not null,
+total decimal (10,2) not null,
+subtotal decimal(10,2) null,
+igv decimal(10,2) null,
+tipocomprobante boolean not null,
+estadopago boolean not null,
+codcliente int not null references cliente(codcliente) 
+)
+
+
+create table detalle(
+numventa int not null references venta(numventa),
+codproducto int not null references producto(codproducto),
+cantidad int not null,
+precioventa decimal(8,2) not null,
+descuento smallint not null,
+subtotal decimal(10,2) not null,
+constraint pk_detalle primary key (numventa,codproducto)
+)
+
